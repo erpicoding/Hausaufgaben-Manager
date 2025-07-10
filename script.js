1da7facacaa8148164fca4db48df24ee07ce5599
erpicoding <159901742+erpicoding@users.noreply.github.com>
2025-07-10 21:34:58 +0000

    beta 0.3

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//srvice-worker ende
+
+//start vom main script
 if (localStorage.getItem("cookieConfirm") == true) {
   console.log("Cookies bestätigt");
 } else if (localStorage.getItem("cookieConfirm") == (false || null)) {
@@ -22,14 +25,12 @@ document.addEventListener("keydown", function (event) {
   }
 });
 
-loadDay("mo");
-loadDay("di");
-loadDay("mi");
-loadDay("do");
-loadDay("fr");
-load();
-
-//if (localStorage.getItem("fertig") == "true")
+buildDay("mo");
+buildDay("di");
+buildDay("mi");
+buildDay("do");
+buildDay("fr");
+loadContent();
 
 function saveStunden() {
   localStorage.setItem("montag", document.getElementById("stundenMo").value);
@@ -45,8 +46,8 @@ function saveStunden() {
   location.reload();
 }
 
-function loadDay(day) {
-  // montag ist die stunden variable und mo setzt den geladenen tag fest
+function buildDay(day) {
+  // montag ist die variable, wie viele stunden am tag sind und mo setzt den geladenen tag fest
   //
 
   var dayID = document.getElementById(day);
@@ -56,29 +57,31 @@ function loadDay(day) {
 
   if (day == "mo") {
     Stunden = localStorage.getItem("montag");
-    h1 = "<h1>Montag</h1>";
+    h1 = "<h2>Montag</h2>";
   } else if (day == "di") {
     Stunden = localStorage.getItem("dienstag");
-    h1 = "<h1>Dienstag</h1>";
+    h1 = "<h2>Dienstag</h2>";
   } else if (day == "mi") {
     Stunden = localStorage.getItem("mittwoch");
-    h1 = "<h1>Mittwoch</h1>";
+    h1 = "<h2>Mittwoch</h2>";
   } else if (day == "do") {
     Stunden = localStorage.getItem("donnerstag");
-    h1 = "<h1>Donnerstag</h1>";
+    h1 = "<h2>Donnerstag</h2>";
   } else if (day == "fr") {
     Stunden = localStorage.getItem("freitag");
-    h1 = "<h1>Freitag</h1>";
+    h1 = "<h2>Freitag</h2>";
   }
 
   dayID.innerHTML = h1;
-
   let i = 1;
   if (Stunden == 1) {
     let fach = document.createElement("div");
     fach.classList.add("fach");
     halbesFach = document.createElement("div");
-    halbesFach.classList.add("halbeFach", day + i);
+    halbesFach.classList.add("halbesFach", day + i);
+    fachInput = document.createElement("input");
+    fachInput.classList.add("fachInput");
+    fachInput.setAttribute("id", "fachInput" + day + i);
     HAinput = document.createElement("input");
     HAinput.setAttribute("id", day + i);
     HAinput.setAttribute("placeholder", "");
@@ -86,6 +89,7 @@ function loadDay(day) {
     removebutton.classList.add("remove");
     removebutton.setAttribute("onclick", "remove(" + day + i + ")");
     removebutton.innerHTML = "X";
+    halbesFach.appendChild(fachInput);
     halbesFach.appendChild(HAinput);
     halbesFach.appendChild(removebutton);
     fach.appendChild(halbesFach);
@@ -97,6 +101,9 @@ function loadDay(day) {
     while (i <= 2) {
       halbesFach = document.createElement("div");
       halbesFach.classList.add("halbesFach", day + i);
+      fachInput = document.createElement("input");
+      fachInput.classList.add("fachInput");
+      fachInput.setAttribute("id", "fachInput" + day + i);
       HAinput = document.createElement("input");
       HAinput.setAttribute("id", day + i);
       HAinput.setAttribute("placeholder", "");
@@ -104,6 +111,7 @@ function loadDay(day) {
       removebutton.classList.add("remove");
       removebutton.setAttribute("onclick", "remove(" + day + i + ")");
       removebutton.innerHTML = "X";
+      halbesFach.appendChild(fachInput);
       halbesFach.appendChild(HAinput);
       halbesFach.appendChild(removebutton);
       fach.appendChild(halbesFach);
@@ -116,6 +124,9 @@ function loadDay(day) {
     fach.classList.add("fach");
     halbesFach = document.createElement("div");
     halbesFach.classList.add("halbesFach", day + i);
+    fachInput = document.createElement("input");
+    fachInput.classList.add("fachInput");
+    fachInput.setAttribute("id", "fachInput" + day + i);
     HAinput = document.createElement("input");
     HAinput.setAttribute("id", day + i);
     HAinput.setAttribute("placeholder", "");
@@ -123,6 +134,7 @@ function loadDay(day) {
     removebutton.classList.add("remove");
     removebutton.setAttribute("onclick", "remove(" + day + i + ")");
     removebutton.innerHTML = "X";
+    halbesFach.appendChild(fachInput);
     halbesFach.appendChild(HAinput);
     halbesFach.appendChild(removebutton);
     fach.appendChild(halbesFach);
@@ -135,6 +147,9 @@ function loadDay(day) {
     while (i <= 4) {
       halbesFach = document.createElement("div");
       halbesFach.classList.add("halbesFach", day + i);
+      fachInput = document.createElement("input");
+      fachInput.classList.add("fachInput");
+      fachInput.setAttribute("id", "fachInput" + day + i);
       HAinput = document.createElement("input");
       HAinput.setAttribute("id", day + i);
       HAinput.setAttribute("placeholder", "");
@@ -142,6 +157,7 @@ function loadDay(day) {
       removebutton.classList.add("remove");
       removebutton.setAttribute("onclick", "remove(" + day + i + ")");
       removebutton.innerHTML = "X";
+      halbesFach.appendChild(fachInput);
       halbesFach.appendChild(HAinput);
       halbesFach.appendChild(removebutton);
       fach.appendChild(halbesFach);
@@ -154,6 +170,9 @@ function loadDay(day) {
     fach.classList.add("fach");
     halbesFach = document.createElement("div");
     halbesFach.classList.add("halbesFach", day + i);
+    fachInput = document.createElement("input");
+    fachInput.classList.add("fachInput");
+    fachInput.setAttribute("id", "fachInput" + day + i);
     HAinput = document.createElement("input");
     HAinput.setAttribute("id", day + i);
     HAinput.setAttribute("placeholder", "");
@@ -161,6 +180,7 @@ function loadDay(day) {
     removebutton.classList.add("remove");
     removebutton.setAttribute("onclick", "remove(" + day + i + ")");
     removebutton.innerHTML = "X";
+    halbesFach.appendChild(fachInput);
     halbesFach.appendChild(HAinput);
     halbesFach.appendChild(removebutton);
     fach.appendChild(halbesFach);
@@ -173,6 +193,9 @@ function loadDay(day) {
     while (i <= 6) {
       halbesFach = document.createElement("div");
       halbesFach.classList.add("halbesFach", day + i);
+      fachInput = document.createElement("input");
+      fachInput.classList.add("fachInput");
+      fachInput.setAttribute("id", "fachInput" + day + i);
       HAinput = document.createElement("input");
       HAinput.setAttribute("id", day + i);
       HAinput.setAttribute("placeholder", "");
@@ -180,6 +203,7 @@ function loadDay(day) {
       removebutton.classList.add("remove");
       removebutton.setAttribute("onclick", "remove(" + day + i + ")");
       removebutton.innerHTML = "X";
+      halbesFach.appendChild(fachInput);
       halbesFach.appendChild(HAinput);
       halbesFach.appendChild(removebutton);
       fach.appendChild(halbesFach);
@@ -192,6 +216,9 @@ function loadDay(day) {
     fach.classList.add("fach");
     halbesFach = document.createElement("div");
     halbesFach.classList.add("halbesFach", day + i);
+    fachInput = document.createElement("input");
+    fachInput.classList.add("fachInput");
+    fachInput.setAttribute("id", "fachInput" + day + i);
     HAinput = document.createElement("input");
     HAinput.setAttribute("id", day + i);
     HAinput.setAttribute("placeholder", "");
@@ -199,6 +226,7 @@ function loadDay(day) {
     removebutton.classList.add("remove");
     removebutton.setAttribute("onclick", "remove(" + day + i + ")");
     removebutton.innerHTML = "X";
+    halbesFach.appendChild(fachInput);
     halbesFach.appendChild(HAinput);
     halbesFach.appendChild(removebutton);
     fach.appendChild(halbesFach);
@@ -211,6 +239,9 @@ function loadDay(day) {
     while (i <= 8) {
       halbesFach = document.createElement("div");
       halbesFach.classList.add("halbesFach", day + i);
+      fachInput = document.createElement("input");
+      fachInput.classList.add("fachInput");
+      fachInput.setAttribute("id", "fachInput" + day + i);
       HAinput = document.createElement("input");
       HAinput.setAttribute("id", day + i);
       HAinput.setAttribute("placeholder", "");
@@ -218,6 +249,7 @@ function loadDay(day) {
       removebutton.classList.add("remove");
       removebutton.setAttribute("onclick", "remove(" + day + i + ")");
       removebutton.innerHTML = "X";
+      halbesFach.appendChild(fachInput);
       halbesFach.appendChild(HAinput);
       halbesFach.appendChild(removebutton);
       fach.appendChild(halbesFach);
@@ -230,6 +262,9 @@ function loadDay(day) {
     fach.classList.add("fach");
     halbesFach = document.createElement("div");
     halbesFach.classList.add("halbesFach", day + i);
+    fachInput = document.createElement("input");
+    fachInput.classList.add("fachInput");
+    fachInput.setAttribute("id", "fachInput" + day + i);
     HAinput = document.createElement("input");
     HAinput.setAttribute("id", day + i);
     HAinput.setAttribute("placeholder", "");
@@ -237,6 +272,7 @@ function loadDay(day) {
     removebutton.classList.add("remove");
     removebutton.setAttribute("onclick", "remove(" + day + i + ")");
     removebutton.innerHTML = "X";
+    halbesFach.appendChild(fachInput);
     halbesFach.appendChild(HAinput);
     halbesFach.appendChild(removebutton);
     fach.appendChild(halbesFach);
@@ -249,6 +285,9 @@ function loadDay(day) {
     while (i <= 10) {
       halbesFach = document.createElement("div");
       halbesFach.classList.add("halbesFach", day + i);
+      fachInput = document.createElement("input");
+      fachInput.classList.add("fachInput");
+      fachInput.setAttribute("id", "fachInput" + day + i);
       HAinput = document.createElement("input");
       HAinput.setAttribute("id", day + i);
       HAinput.setAttribute("placeholder", "");
@@ -256,6 +295,7 @@ function loadDay(day) {
       removebutton.classList.add("remove");
       removebutton.setAttribute("onclick", "remove(" + day + i + ")");
       removebutton.innerHTML = "X";
+      halbesFach.appendChild(fachInput);
       halbesFach.appendChild(HAinput);
       halbesFach.appendChild(removebutton);
       fach.appendChild(halbesFach);
@@ -296,20 +336,32 @@ function changeColor(input) {
   });
 }
 
-function load() {
+function loadContent() {
   //
+
+  document.getElementById("stundenMo").value = localStorage.getItem("montag");
+  document.getElementById("stundenDi").value = localStorage.getItem("dienstag");
+  document.getElementById("stundenMi").value = localStorage.getItem("mittwoch");
+  document.getElementById("stundenDo").value =
+    localStorage.getItem("donnerstag");
+  document.getElementById("stundenFr").value = localStorage.getItem("freitag");
+
   loadMo();
   loadDi();
   loadMi();
   loadDo();
   loadFr();
 
-  //old:
+  //
   function loadMo() {
     montag = localStorage.getItem("montag");
     i = 1;
     while (i <= montag - 1) {
       document.getElementById("mo" + i).value = localStorage.getItem("mo" + i);
+      document.getElementById("fachInputmo" + i).value = localStorage.getItem(
+        "fachInputmo" + i
+      );
+      console.log("mo" + i + " geladen");
       let colorFeld = document.querySelectorAll(".mo" + i);
       let color = localStorage.getItem("colormo" + i);
       colorFeld.forEach((el) => {
@@ -323,6 +375,9 @@ function load() {
     i = 1;
     while (i <= dienstag - 1) {
       document.getElementById("di" + i).value = localStorage.getItem("di" + i);
+      document.getElementById("fachInputdi" + i).value = localStorage.getItem(
+        "fachInputdi" + i
+      );
       let colorFeld = document.querySelectorAll(".di" + i);
       let color = localStorage.getItem("colordi" + i);
       colorFeld.forEach((el) => {
@@ -336,6 +391,9 @@ function load() {
     i = 1;
     while (i <= mittwoch - 1) {
       document.getElementById("mi" + i).value = localStorage.getItem("mi" + i);
+      document.getElementById("fachInputmi" + i).value = localStorage.getItem(
+        "fachInputmi" + i
+      );
       let colorFeld = document.querySelectorAll(".mi" + i);
       let color = localStorage.getItem("colormi" + i);
       colorFeld.forEach((el) => {
@@ -349,6 +407,9 @@ function load() {
     i = 1;
     while (i <= donnerstag - 1) {
       document.getElementById("do" + i).value = localStorage.getItem("do" + i);
+      document.getElementById("fachInputdo" + i).value = localStorage.getItem(
+        "fachInputdo" + i
+      );
       let colorFeld = document.querySelectorAll(".do" + i);
       let color = localStorage.getItem("colordo" + i);
       colorFeld.forEach((el) => {
@@ -362,6 +423,9 @@ function load() {
     i = 1;
     while (i <= freitag) {
       document.getElementById("fr" + i).value = localStorage.getItem("fr" + i);
+      document.getElementById("fachInputfr" + i).value = localStorage.getItem(
+        "fachInputfr" + i
+      );
       let colorFeld = document.querySelectorAll(".fr" + i);
       let color = localStorage.getItem("colorfr" + i);
       colorFeld.forEach((el) => {
@@ -437,9 +501,8 @@ function saveAll() {
     if (feld != null) {
       if (feld.value.length > 1) {
         localStorage.setItem(feld.id, feld.value);
-        console.log("gespeichert: " + feld.id + " inhalt: " + feld.value);
-        feld.value = localStorage.getItem(feld.id);
-        feld.placeholder = localStorage.getItem(feld.id);
+        let fachInput = document.getElementById("fachInput" + feld.id);
+        localStorage.setItem("fachInput" + feld.id, fachInput.value);
       }
     }
   }
@@ -483,6 +546,11 @@ function removeAll(askForConfirm) {
         remove(feld);
         i++;
       }
+      function remove(feld) {
+        localStorage.setItem(feld.id, "");
+        feld.placeholder = feld.value;
+        feld.value = "";
+      }
     }
   } else {
     montag = localStorage.getItem("montag");
@@ -520,12 +588,7 @@ function removeAll(askForConfirm) {
       fullRemove(feld);
       i++;
     }
-
     function remove(feld) {
-      localStorage.setItem(feld.id, "");
-      feld.placeholder = "";
-    }
-    function fullRemove(feld) {
       localStorage.setItem(feld.id, "");
       feld.value = "";
       feld.placeholder = "";
@@ -534,6 +597,6 @@ function removeAll(askForConfirm) {
 }
 function remove(feld) {
   localStorage.setItem(feld.id, "");
+  feld.placeholder = feld.value;
   feld.value = "";
 }
-
